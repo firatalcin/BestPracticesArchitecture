9eb3d2058d8c05561dd3792e63c71f8ed5af56ac
Fırat Alçın <firatalcin04@gmail.com>
2025-08-11 23:03:05 +0300

    Repositories(Migration)

---
+++ BestPracticesProject/BestPracticesProject.Repositories/ConnectionStringOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestPracticesProject.Repositories
+{
+    public class ConnectionStringOption
+    {
+        public const string Key = "ConnectionStrings";
+        public string SqlConnection { get; set; } = default!;
+    }
+}
