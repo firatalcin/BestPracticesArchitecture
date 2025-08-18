8e3399af8b9e7afa7cfec0fc20fd73e962f4b10b
Fırat Alçın <firatalcin04@gmail.com>
2025-08-18 22:10:41 +0300

    Product Repository

--- BestPracticesProject/BestPracticesProject.Repositories/Products/Product.cs
+++ BestPracticesProject/BestPracticesProject.Repositories/Products/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BestPracticesProject.Repositories.Products
 {
-    public class Product
+    public class Product : BaseEntity<int>
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
