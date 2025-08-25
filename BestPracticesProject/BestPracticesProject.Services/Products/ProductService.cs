c7dc94073123d09b50b02db1a2dc894eab389b69
Fırat Alçın <firatalcin04@gmail.com>
2025-08-25 22:52:39 +0300

    Result Pattern

--- BestPracticesProject/BestPracticesProject.Services/Products/ProductService.cs
+++ BestPracticesProject/BestPracticesProject.Services/Products/ProductService.cs
@@ -9,5 +9,20 @@ namespace BestPracticesProject.Services.Products
 {
     public class ProductService(IProductRepository productRepository) : IProductService
     {
+        public async Task<ServiceResult<List<Product>>> GetTopPriceProductsAsync(int count)
+        {
+            var products = await productRepository.GetTopPriceProductsAsync(count);
+            return new ServiceResult<List<Product>> { Data = products };
+        }
+
+        public async Task<ServiceResult<Product?>> GetByIdAsync(int id)
+        {
+            var product = await productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return new ServiceResult<Product?> { ErrorMessage = new List<string> { "Product not found" } };
+            }
+            return new ServiceResult<Product?> { Data = product };
+        }
     }
 }
---
+++ BestPracticesProject/BestPracticesProject.Services/ServiceResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BestPracticesProject.Services
+{
+    public class ServiceResult<T>
+    {
+        public T? Data { get; set; }
+        public List<string>? ErrorMessage { get; set; }
+        [JsonIgnore] public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
+        [JsonIgnore] public bool IsFail => !IsSuccess;
+        [JsonIgnore] public HttpStatusCode Status { get; set; }
+
+        [JsonIgnore] public string? UrlAsCreated { get; set; }
+
+        //static factory method
+        public static ServiceResult<T> Success(T data, HttpStatusCode status = HttpStatusCode.OK)
+        {
+            return new ServiceResult<T>()
+            {
+                Data = data,
+                Status = status
+            };
+        }
+
+        public static ServiceResult<T> SuccessAsCreated(T data, string urlAsCreated)
+        {
+            return new ServiceResult<T>()
+            {
+                Data = data,
+                Status = HttpStatusCode.Created,
+                UrlAsCreated = urlAsCreated
+            };
+        }
+
+
+        public static ServiceResult<T> Fail(List<string> errorMessage,
+            HttpStatusCode status = HttpStatusCode.BadRequest)
+        {
+            return new ServiceResult<T>()
+            {
+                ErrorMessage = errorMessage,
+                Status = status
+            };
+        }
+
+        public static ServiceResult<T> Fail(string errorMessage, HttpStatusCode status = HttpStatusCode.BadRequest)
+        {
+            return new ServiceResult<T>()
+            {
+                ErrorMessage = [errorMessage],
+                Status = status
+            };
+        }
+    }
+    
+}
