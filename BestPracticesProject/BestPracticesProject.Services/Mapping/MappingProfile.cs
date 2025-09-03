e507231eeb940cdb8fec5d3097ee34530206d5c6
Fırat Alçın <firatalcin04@gmail.com>
2025-09-03 19:15:21 +0300

    Product Add/Update Mapping

--- BestPracticesProject/BestPracticesProject.Services/Mapping/MappingProfile.cs
+++ BestPracticesProject/BestPracticesProject.Services/Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BestPracticesProject.Repositories.Products;
 using BestPracticesProject.Services.Products;
+using BestPracticesProject.Services.Products.Create;
+using BestPracticesProject.Services.Products.Update;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,12 @@ namespace BestPracticesProject.Services.Mapping
         public MappingProfile()
         {
             CreateMap<Product, ProductDto>().ReverseMap();
+
+            CreateMap<CreateProductRequest, Product>().ForMember(dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name.ToLowerInvariant()));
+
+            CreateMap<UpdateProductRequest, Product>().ForMember(dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name.ToLowerInvariant()));
         }
     }
 }
